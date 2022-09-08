d4ee1ef16da40fb64e6748544807734720197a77
anton2920 <anton2920@gmail.com>
2022-09-08 19:26:40 +0300

    Added programs from the first chapter

---
+++ training/gobook/ch01/fetchall.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+
+	start := time.Now()
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) /* Start a goroutine */
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) /* Receive from channel ch */
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		secs := time.Since(start).Seconds()
+		ch <- fmt.Sprintf("%.2fs %v", secs, err) /* Send to channel 'ch' */
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() /* Don't leak resources */
+	if err != nil {
+		secs := time.Since(start).Seconds()
+		ch <- fmt.Sprintf("%.2fs while reading %s: %v", secs, url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
