e6f6be56db80cf453ad05c5b240a3f23d1e9bbfd
anton2920 <anton2920@gmail.com>
2026-08-21 16:32:02 +0300

    home/junior: updated program to test new binary.Tree

--- home/junior/main.go
+++ home/junior/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"runtime"
 	"unsafe"
 
 	"github.com/anton2920/gofa/container/binary"
 	"github.com/anton2920/gofa/container/linked"
 	"github.com/anton2920/gofa/context"
 	"github.com/anton2920/gofa/context/context_"
+	"github.com/anton2920/gofa/cpu"
 	_ "github.com/anton2920/gofa/debug/debug_"
 	"github.com/anton2920/gofa/event"
 	"github.com/anton2920/gofa/event/event_"
@@ -31,8 +31,8 @@ func ToIntListNode(n *linked.ListNode) *IntListNode {
 	return (*IntListNode)(unsafe.Pointer(n))
 }
 
-func NewIntListNode(ll *linked.List, value int) *linked.ListNode {
-	n := ToIntListNode(ll.NewNode())
+func NewIntListNode(ctx *context.Context, value int) *linked.ListNode {
+	n := (*IntListNode)(ctx.Arena.PushSizeWithAlignment(unsafe.Sizeof(IntListNode{}), unsafe.Alignof(IntListNode{})))
 	n.Value = value
 	return (*linked.ListNode)(unsafe.Pointer(n))
 }
@@ -46,8 +46,8 @@ func ToIntTreeNode(n *binary.TreeNode) *IntTreeNode {
 	return (*IntTreeNode)(unsafe.Pointer(n))
 }
 
-func NewIntTreeNode(bt *binary.SearchTree, value int) *binary.TreeNode {
-	n := ToIntTreeNode(bt.NewNode())
+func NewIntTreeNode(ctx *context.Context, value int) *binary.TreeNode {
+	n := (*IntTreeNode)(ctx.Arena.PushSizeWithAlignment(unsafe.Sizeof(IntTreeNode{}), unsafe.Alignof(IntTreeNode{})))
 	n.Value = value
 	return (*binary.TreeNode)(unsafe.Pointer(n))
 }
@@ -70,10 +70,40 @@ func traverseTreeNodesInOrder(ctx *context.Context, n *IntTreeNode) {
 	}
 }
 
-func TraverseTreeInOrder(ctx *context.Context, bt *binary.SearchTree) {
+func PrintTreeInOrder(ctx *context.Context, bt *binary.SearchTree) {
 	ctx.Fmt.Reset()
 	traverseTreeNodesInOrder(ctx, ToIntTreeNode(bt.Root))
 	fmt_.Println(ctx, ctx.Fmt.Backspace(2))
+}
+
+func traverseTreeNodesPreOrderPretty(ctx *context.Context, n *IntTreeNode, level int) {
+	if n == nil {
+		for i := 0; i < level; i++ {
+			ctx.Fmt.S("\t")
+		}
+		ctx.Fmt.S("nil").Ln()
+	} else {
+		for i := 0; i < level; i++ {
+			ctx.Fmt.S("\t")
+		}
+		ctx.Fmt.D(n.Value).Ln()
+
+		traverseTreeNodesPreOrderPretty(ctx, ToIntTreeNode(n.Left), level+1)
+		traverseTreeNodesPreOrderPretty(ctx, ToIntTreeNode(n.Right), level+1)
+	}
+}
+
+func PrintTreePreOrderPretty(ctx *context.Context, bt *binary.SearchTree) {
+	ctx.Fmt.Reset()
+	traverseTreeNodesPreOrderPretty(ctx, ToIntTreeNode(bt.Root), 0)
+	fmt_.Println(ctx, &ctx.Fmt)
+}
+
+func Plural(n int, s string) string {
+	if n == 1 {
+		return s[:len(s)-1]
+	}
+	return s
 }
 
 func Main(ctx *context.Context) {
@@ -85,9 +115,8 @@ func Main(ctx *context.Context) {
 		save := ctx.Arena
 
 		ll := (*linked.List)(ctx.Arena.PushSizeWithAlignment(unsafe.Sizeof(linked.List{}), unsafe.Alignof(linked.List{})))
-		ll.Init(&ctx.Arena, unsafe.Sizeof(0), unsafe.Alignof(0))
 		for i := 0; i < 10; i++ {
-			ll.Append(NewIntListNode(ll, i))
+			ll.Append(NewIntListNode(ctx, i))
 		}
 
 		f := ctx.Fmt.Reset().S("[")
@@ -109,14 +138,37 @@ func Main(ctx *context.Context) {
 		save := ctx.Arena
 
 		bt := (*binary.SearchTree)(ctx.Arena.PushSizeWithAlignment(unsafe.Sizeof(binary.SearchTree{}), unsafe.Alignof(binary.SearchTree{})))
-		bt.Init(&ctx.Arena, unsafe.Sizeof(0), unsafe.Alignof(0), func(a *binary.TreeNode, b *binary.TreeNode) int {
-			return ToIntTreeNode(a).Value - ToIntTreeNode(b).Value
-		})
-
-		for i := 0; i < 10; i++ {
-			bt.Insert(NewIntTreeNode(bt, i))
+		bt.Comparator = func(a binary.TreeKeyPointer, b binary.TreeKeyPointer) int {
+			return *(*int)(a) - *(*int)(b)
 		}
-		TraverseTreeInOrder(ctx, bt)
+
+		keys := ctx.Arena.PushIntArray(10)
+		for i := 0; i < len(keys); i++ {
+			n, _ := cpu.ReadRandomUint16()
+			keys[i] = int(n % 100)
+			bt.Add(NewIntTreeNode(ctx, keys[i]))
+		}
+
+		duplicates := len(keys) - bt.N
+		if duplicates > 0 {
+			//fmt_.Println(ctx, ctx.Fmt.Reset().S("Generated ").D(duplicates).S(" ").S(Plural(duplicates, "duplicates")))
+		}
+
+		for i := 0; i < len(keys); i++ {
+			context_.Must(ctx, bt.Has(binary.TreeKeyPointer(&keys[i])), "Failed to find existing key in the tree")
+		}
+
+		PrintTreeInOrder(ctx, bt)
+		for i := 0; i < len(keys); i++ {
+			//PrintTreeInOrder(ctx, bt)
+			//PrintTreePreOrderPretty(ctx, bt)
+			//fmt_.Println(ctx, ctx.Fmt.Reset().S("Removing ").D(keys[i]).S("..."))
+			if !bt.Has(binary.TreeKeyPointer(&keys[i])) {
+				duplicates--
+			}
+			context_.Must(ctx, (bt.Del(binary.TreeKeyPointer(&keys[i])) != nil) || (duplicates >= 0), "Failed to delete existing key in the tree")
+		}
+		//PrintTreeInOrder(ctx, bt)
 
 		ctx.Arena = save
 	}
@@ -174,8 +226,8 @@ func Main(ctx *context.Context) {
 					if e.EndOfFile() {
 						continue
 					}
-
 					c := os.Handle(e.Identifier)
+
 					n, ok := os.ReadFromFile(ctx, c, buf.RemainingSlice())
 					if n == 0 {
 						continue
@@ -184,6 +236,7 @@ func Main(ctx *context.Context) {
 						fmt_.Eprintln(ctx, ctx.Fmt.Reset().S("Failed to read data from client: ").S(ctx.Error()))
 					}
 					buf.Produce(n)
+
 					m, ok := os.WriteToFile(ctx, c, buf.UnconsumedSlice())
 					if m == 0 {
 						continue
@@ -197,19 +250,26 @@ func Main(ctx *context.Context) {
 				fmt_.Println(ctx, ctx.Fmt.Reset().S("Received signal ").D(int(e.Identifier)).S(" (").S(os.Signal(e.Identifier).String()).S("). Exitting..."))
 				quit = true
 			case os.EventTypeTimer:
-				now := time_.NowInNanoseconds()
+				//now := time_.NowInNanoseconds()
+				save := ctx.Arena
+
+				t := (*os.SecondsWithNanoseconds)(ctx.Arena.PushSizeWithAlignment(2*unsafe.Sizeof(0), unsafe.Alignof(0)))
+				_ = os.GetCurrentTime(ctx, t)
+				now := int64(t.Seconds)*time.Second + int64(t.Nanoseconds)
 				if e.Identifier == 1 {
 					fmt_.Println(ctx, ctx.Fmt.Reset().S("Current time: ").DateTime(now))
 				} else {
 					fmt_.Println(ctx, ctx.Fmt.Reset().S("TIMER FIRED AT: ").DateTime(now))
 				}
+
+				ctx.Arena = save
 			}
 		}
 	}
 }
 
 func main() {
-	runtime.AllocationsAreDisabled = true
+	//runtime.AllocationsAreDisabled = true
 
 	var ctx context.Context
 	context_.Must(&ctx, context_.BootstrapWithEvenlySplitSize(&ctx, ints.GiB(1)), "Failed to bootstrap context")
